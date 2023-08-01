1de3e92cf4125545320f0f8dc78c96f6bb8df476
nkiyo <nakahara.kiyoshi@gmail.com>
2023-08-01 09:01:34 +0900

    init commit

---
+++ sample.js
@@ -0,0 +1,26 @@
+const _ = require('lodash')
+
+// sortBy
+var users = [
+  { 'user': 'fred',   'age': 48 },
+  { 'user': 'barney', 'age': 36 },
+  { 'user': 'fred',   'age': 40 },
+  { 'user': 'barney', 'age': 34 }
+];
+sortedUsers = _.sortBy(users, ['age', 'name'])
+console.log(sortedUsers)
+//console.log(users) // 順序は変更されていない
+
+// groupBy
+// 年代別にグループ分けする
+groupedUsers = _.groupBy(users, e => Math.floor(e.age / 10))
+console.log(groupedUsers)
+
+// difference 差集合
+a1 = [2, 1, 4]
+a2 = [3, 2, 5]
+diffa1a2 = _.difference(a1, a2)
+diffa2a1 = _.difference(a2, a1)
+console.log(diffa1a2)
+console.log(diffa2a1)
+
