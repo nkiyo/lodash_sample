a082f36e3008540203fea501e3c0b3d49a20ed22
nkiyo <nakahara.kiyoshi@gmail.com>
2023-08-02 19:39:57 +0900

    up

--- sample.js
+++ sample.js
@@ -1,12 +1,18 @@
 const _ = require('lodash')
 
+// TODO Typescript化
+
 // sortBy
-var users = [
-  { 'user': 'fred',   'age': 48 },
-  { 'user': 'barney', 'age': 36 },
-  { 'user': 'fred',   'age': 40 },
-  { 'user': 'barney', 'age': 34 }
+const users = [
+  { 'user': 'fred',   'age': 48, 'gender': 1 },
+  { 'user': 'barney', 'age': 36, 'gender': 2 },
+  { 'user': 'fred',   'age': 40, 'gender': 3 },
+  { 'user': 'barney', 'age': 34, 'gender': 2 }
 ];
+const genders = [
+  { 'id': 1, 'gender': 'male'},
+  { 'id': 2, 'gender': 'female'},
+]
 sortedUsers = _.sortBy(users, ['age', 'name'])
 console.log(sortedUsers)
 //console.log(users) // 順序は変更されていない
@@ -33,7 +39,26 @@ intersectionA1a2 = _.intersection(a1, a2)
 console.log(intersectionA1a2)
 
 // intersectionWith, intersectionBy もあり。unionも同様
+
 // xor
+xorA1a2 = _.xor(a1, a2)
+console.log(xorA1a2)
+
 // zip
+
 // countBy
+// groupByと、ほぼ同じ
+countUsers = _.countBy(users, e => Math.floor(e.age / 10))
+console.log(countUsers)
+
 // SQLのjoinに近いやつは？
+
+// idを詳細情報に変換する
+idConvertedUsers = users.map(e => {
+  genderStr = genders.find(e2 => e2.id === e.gender) ?? '-'
+  return {...e, 'gender': genderStr}
+})
+//idConvertedUsers = users.map((e) => { return {...e, 'gender': 'xxx'} })
+console.log(idConvertedUsers)
+
+
